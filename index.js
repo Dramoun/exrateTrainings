3b8013d10fb3490c4288415d461b1cc8ac53ae05
Jan Holub <64842952+Dramoun@users.noreply.github.com>
2022-08-15 11:22:41 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,199 @@
+//python3 -m http.server
+
+//var jsonData = JSON.parse('[{"shortName":"AUD","validFrom":"2022-04-26T00:00:00","name":"Dollar","country":"Australia","move":-0.5,"amount":1,"valBuy":15.74,"valSell":16.88,"valMid":16.306,"currBuy":15.898,"currSell":16.714,"currMid":16.306,"version":1,"cnbMid":16.31,"ecbMid":1.497},{"shortName":"ZAR","validFrom":"2022-04-26T00:00:00","name":"Rand","country":"South Africa","move":0,"amount":1,"valBuy":0,"valSell":0,"valMid":0,"currBuy":1.409,"currSell":1.482,"currMid":1.445,"version":1,"cnbMid":1.449,"ecbMid":16.855},{"shortName":"CAD","validFrom":"2022-04-26T00:00:00","name":"Dollar","country":"Canada","move":0.52,"amount":1,"valBuy":17.24,"valSell":18.49,"valMid":17.865,"currBuy":17.418,"currSell":18.312,"currMid":17.865,"version":1,"cnbMid":17.814,"ecbMid":1.371},{"shortName":"CHF","validFrom":"2022-04-26T00:00:00","name":"Franc","country":"Switzerland","move":1.13,"amount":1,"valBuy":22.99,"valSell":24.65,"valMid":23.82,"currBuy":23.224,"currSell":24.415,"currMid":23.82,"version":1,"cnbMid":23.784,"ecbMid":1.027},{"shortName":"DKK","validFrom":"2022-04-26T00:00:00","name":"Krone","country":"Denmark","move":0.37,"amount":1,"valBuy":3.17,"valSell":3.4,"valMid":3.284,"currBuy":3.202,"currSell":3.366,"currMid":3.284,"version":1,"cnbMid":3.283,"ecbMid":7.439},{"shortName":"EUR","validFrom":"2022-04-26T00:00:00","name":"Euro","country":"EU","move":0.37,"amount":1,"valBuy":23.57,"valSell":25.29,"valMid":24.43,"currBuy":23.819,"currSell":25.041,"currMid":24.43,"version":1,"cnbMid":24.42,"ecbMid":0},{"shortName":"GBP","validFrom":"2022-04-26T00:00:00","name":"Pound","country":"Great Britain","move":0.03,"amount":1,"valBuy":28.01,"valSell":30.04,"valMid":29.028,"currBuy":28.303,"currSell":29.754,"currMid":29.028,"version":1,"cnbMid":28.956,"ecbMid":0.843},{"shortName":"HKD","validFrom":"2022-04-26T00:00:00","name":"Dollar","country":"Hong Kong","move":1.04,"amount":1,"valBuy":0,"valSell":0,"valMid":0,"currBuy":2.832,"currSell":2.978,"currMid":2.905,"version":1,"cnbMid":2.896,"ecbMid":8.433},{"shortName":"HRK","validFrom":"2022-04-26T00:00:00","name":"Kuna","country":"Croatia","move":0.37,"amount":1,"valBuy":3.12,"valSell":3.34,"valMid":3.23,"currBuy":3.149,"currSell":3.311,"currMid":3.23,"version":1,"cnbMid":3.23,"ecbMid":7.562},{"shortName":"HUF","validFrom":"2022-04-26T00:00:00","name":"Forint","country":"Hungary","move":-0.37,"amount":100,"valBuy":6.3,"valSell":6.76,"valMid":6.531,"currBuy":6.368,"currSell":6.694,"currMid":6.531,"version":1,"cnbMid":6.528,"ecbMid":37408},{"shortName":"JPY","validFrom":"2022-04-26T00:00:00","name":"Yen","country":"Japan","move":1.36,"amount":100,"valBuy":17.2,"valSell":18.45,"valMid":17.823,"currBuy":17.377,"currSell":18.268,"currMid":17.823,"version":1,"cnbMid":17.729,"ecbMid":13773},{"shortName":"NOK","validFrom":"2022-04-26T00:00:00","name":"Krone","country":"Norway","move":-0.99,"amount":1,"valBuy":2.41,"valSell":2.59,"valMid":2.499,"currBuy":2.437,"currSell":2.562,"currMid":2.499,"version":1,"cnbMid":2.517,"ecbMid":9.702},{"shortName":"NZD","validFrom":"2022-04-26T00:00:00","name":"Dollar","country":"New Zealand","move":0.53,"amount":1,"valBuy":0,"valSell":0,"valMid":0,"currBuy":14.674,"currSell":15.426,"currMid":15.05,"version":1,"cnbMid":15.037,"ecbMid":1.624},{"shortName":"PLN","validFrom":"2022-04-26T00:00:00","name":"Złoty","country":"Poland","move":0.31,"amount":1,"valBuy":5.07,"valSell":5.44,"valMid":5.255,"currBuy":5.123,"currSell":5.386,"currMid":5.255,"version":1,"cnbMid":5.264,"ecbMid":4.64},{"shortName":"RON","validFrom":"2022-04-26T00:00:00","name":"Leu","country":"Romania","move":0.33,"amount":1,"valBuy":0,"valSell":0,"valMid":0,"currBuy":4.816,"currSell":5.063,"currMid":4.939,"version":1,"cnbMid":4.938,"ecbMid":4.946},{"shortName":"SEK","validFrom":"2022-04-26T00:00:00","name":"Krona","country":"Sweden","move":-0.59,"amount":1,"valBuy":2.27,"valSell":2.44,"valMid":2.354,"currBuy":2.295,"currSell":2.413,"currMid":2.354,"version":1,"cnbMid":2.36,"ecbMid":10.348},{"shortName":"TND","validFrom":"2022-04-26T00:00:00","name":"Dinar","country":"Tunisia","move":-0.25,"amount":1,"valBuy":0,"valSell":0,"valMid":0,"currBuy":7.284,"currSell":7.657,"currMid":7.471,"version":1,"cnbMid":0,"ecbMid":0},{"shortName":"TRY","validFrom":"2022-04-26T00:00:00","name":"Lira","country":"Turkey","move":0.92,"amount":1,"valBuy":0,"valSell":0,"valMid":0,"currBuy":1.504,"currSell":1.581,"currMid":1.543,"version":1,"cnbMid":1.539,"ecbMid":15.864},{"shortName":"USD","validFrom":"2022-04-26T00:00:00","name":"Dollar","country":"USA","move":1.07,"amount":1,"valBuy":22,"valSell":23.6,"valMid":22.798,"currBuy":22.228,"currSell":23.368,"currMid":22.798,"version":1,"cnbMid":22.725,"ecbMid":1.075},{"shortName":"BGN","validFrom":"2022-04-26T00:00:00","name":"Lev","country":"Bulgaria","move":0.35,"amount":1,"valBuy":0,"valSell":0,"valMid":0,"currBuy":12.177,"currSell":12.802,"currMid":12.489,"version":1,"cnbMid":12.486,"ecbMid":1.956}]')
+var fromDate = new Date().toISOString().slice(0, 10)
+//console.log(fromDate)
+
+//API call to get exchange rate data (today's date)
+function GetRateData(date){
+  //UpdateTable(jsonData);
+
+  var request = new XMLHttpRequest();
+  const url = 'https://webapi.developers.erstegroup.com/api/csas/public/sandbox/v2/rates/exchangerates/?fromDate='+date+'&toDate=&curr=&lang='
+
+  request.onreadystatechange = function() {
+    if (this.readyState === 4) {
+      //console.log('Status:', this.status);
+      //console.log('Headers:', this.getAllResponseHeaders());
+      var jsonData = JSON.parse(this.responseText);
+      //console.log('Response: ', jsonData[0]);
+      UpdateTable(jsonData);
+    }
+  };
+  request.open('GET', url);
+  request.setRequestHeader('WEB-API-key', 'c52a0682-4806-4903-828f-6cc66508329e');
+  request.send();
+
+};
+
+//Funtion for table creation (useable data from API call)
+function UpdateTable(cleanData){
+  var jsonData = cleanData
+  var tableSpace = document.getElementById('tableSpace');
+  var table = document.createElement('table');
+  table.setAttribute("class","tableClass")
+  var thead = document.createElement('thead');
+  var tbody = document.createElement('tbody');
+
+  //Creating main headers
+  var hrow = document.createElement("tr");
+  for (var i = 0; i < 6; i++) {
+    if (i == 0) {
+      var valName = "Country"
+    }else if (i == 1) {
+      var valName = "Short Name"
+    }else if(i == 2) {
+      var valName = "Current buy rate"
+    }else if(i == 3) {
+      var valName = "Median sell rate"
+    }else if (i == 4) {
+      var valName = "Current sell rate"
+    }else {
+      var valName = "More details"
+    };
+    var hcol = document.createElement("td");
+    //Main headers cell styling
+    hcol.style.cssText = 'padding: 5px;font-size:16px;font-weight: bold;border-bottom:1px solid darkgray';
+    hcol.appendChild(document.createTextNode(valName));
+    hrow.appendChild(hcol);
+  };
+  thead.appendChild(hrow);
+
+  //Creating main rows
+  for (var row in jsonData) {
+    var trow = document.createElement("tr");
+    var rateBool = false;
+    var buttonBool = false;
+    for (var i = 0; i < 6; i++) {
+      if (i == 0) {
+        var valName = "country"
+      }else if (i == 1) {
+        var valName = "shortName"
+      }else if (i == 2) {
+        var valName = "currBuy"
+        var rateBool = true
+      }else if (i == 3){
+        var valName = "currMid"
+      }else if (i == 4){
+        var valName = "currSell"
+      }else {
+        var buttonBool = true
+      };
+      var tcol = document.createElement("td");
+      if (buttonBool) {
+        const detailBtn = document.createElement("button");
+        detailBtn.innerHTML = "+";
+        detailBtn.setAttribute("id", jsonData[row]["shortName"]+"button");
+
+        detailBtn.addEventListener("click", function onClick(event){
+
+          var detailRef = Array.from(document.getElementsByClassName((event.target.id).substr(0,3)+"details"))
+          detailRef.forEach(item => {
+            if (item.style.display == "none"){
+              item.style.display = "table-row";
+              detailBtn.innerHTML = "-";
+            }else{
+              item.style.display = "none";
+              detailBtn.innerHTML = "+";
+            };
+          });
+        });
+        tcol.appendChild(detailBtn);
+        tcol.style.cssText = 'padding: 5px;text-align:center;border-top:1px solid darkgray';
+        trow.appendChild(tcol);
+      }else {
+        if (rateBool) {
+          tcol.appendChild(document.createTextNode(jsonData[row][valName]+"Czk"));
+        }else {
+          tcol.appendChild(document.createTextNode(jsonData[row][valName]));
+        };
+        tcol.style.cssText = 'padding: 5px;font-size:15px;border-top:1px solid darkgray';
+        trow.appendChild(tcol);
+      };
+      //Main rows cell styling
+
+    };
+    tbody.appendChild(trow);
+
+    //Creating detail headers
+    var detailhrow = document.createElement("tr");
+    for (var i = 0; i < 5; i++) {
+      if (i == 0) {
+        var valName = "Name"
+      }else if (i == 1) {
+        var valName = "valBuy"
+      }else if (i == 2) {
+        var valName = "valMid"
+      }else if (i == 3) {
+        var valName = "valSell"
+      }else {
+        var valName = "Valid from"
+      };
+      var detailhcol = document.createElement("td");
+      detailhcol.appendChild(document.createTextNode(valName));
+      //Detail headers cell styling
+      detailhcol.style.cssText = 'padding: 5px;font-size:14px;font-weight: bold;';
+      detailhrow.appendChild(detailhcol);
+    };
+    detailhrow.classList.add(jsonData[row]["shortName"]+"details");
+    detailhrow.style.display = "none";
+    tbody.appendChild(detailhrow);
+
+    //Creating detail rows
+    var detailtrow = document.createElement("tr");
+    var seCrateBool = false;
+    for (var i = 0; i < 5; i++) {
+      if (i == 0) {
+        var valName = "name"
+      }else if (i == 1) {
+        var valName = "valBuy"
+        var seCrateBool = true
+      }else if (i == 2) {
+        var valName = "valMid"
+      }else if (i == 3){
+        var valName = "valSell"
+      }else {
+        var valName = "validFrom"
+        var seCrateBool = false
+      };
+      var detailtcol = document.createElement("td");
+      if (seCrateBool) {
+        detailtcol.appendChild(document.createTextNode(jsonData[row][valName]+"Czk"));
+      }else {
+        if (valName == "validFrom") {
+          var dateValue = jsonData[row][valName].substr(0,10)
+          detailtcol.appendChild(document.createTextNode(dateValue));
+        }else {
+          detailtcol.appendChild(document.createTextNode(jsonData[row][valName]));
+        }
+      };
+      //Detail row cell styling
+      detailtcol.style.cssText = 'padding: 5px;font-size:13px;';
+      detailtrow.appendChild(detailtcol);
+    };
+    detailtrow.classList.add(jsonData[row]["shortName"]+"details");
+    detailtrow.style.display = "none";
+    tbody.appendChild(detailtrow);
+  };
+
+  // Adding the entire table to the tableSpace div
+  table.appendChild(thead);
+  table.appendChild(tbody);
+  tableSpace.appendChild(table);
+};
+
+//On website load start getting data for the table
+window.onload = function(){
+  GetRateData(fromDate);
+};
+
+//Unnecessarily complicated button for reloading the page
+var buttonDiv = document.getElementById('refreshButton');
+var refBtn = document.createElement("button");
+refBtn.innerHTML = "Refresh";
+
+refBtn.addEventListener("click", function onClick(){
+  window.location.reload();
+});
+
+buttonDiv.appendChild(refBtn);
